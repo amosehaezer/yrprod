3c88c958637c1422d57bcc9903ac03041bc1854a
Amos Ebenhaezer <amosehaezer@gmail.com>
2019-10-04 08:12:53 +0700

    first commit

---
+++ resources/views/errors/404.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Page not found - 404</title>
+</head>
+<body>
+ 
+ 
+{{--  The page your looking for is not available  --}}
+<h1 class="align-center">Halaman ini dapat diakses H-1 minggu sebelum acara</h1>
+ 
+</body>
+</html>
